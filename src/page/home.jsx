3d39081d382db04c60a3a10799731f13edb49bf6
seng long <hahura70@gmail.com>
2026-07-13 00:47:36 +0700

    homepage navigation

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -7,13 +7,13 @@ import { Link } from "react-router-dom";
 export default function Homepage() {
   const menProducts = products.filter(product => product.category === "Man");
   console.log("Men category", menProducts);
-function scrollToTop() {
-  // This scrolls the window to the very top (X: 0, Y: 0) smoothly
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-}
+  function scrollToTop() {
+    // This scrolls the window to the very top (X: 0, Y: 0) smoothly
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
   return (
     <div className="bg-gray-50 min-h-screen">
 
@@ -39,9 +39,12 @@ function scrollToTop() {
           </p>
 
           <div className="flex gap-4 mt-8">
-            <button className="bg-black text-white px-8 py-3 rounded-lg hover:bg-gray-800">
-              Shop Now
-            </button>
+            <Link to={`/produst`} onClick={scrollToTop}>
+
+              <button className="bg-black text-white px-8 py-3 rounded-lg hover:bg-gray-800">
+                Shop Now
+              </button>
+            </Link>
 
             <button className="border border-black px-8 py-3 rounded-lg hover:bg-black hover:text-white">
               Explore
@@ -69,11 +72,11 @@ function scrollToTop() {
         <div className="grid md:grid-cols-3 gap-8 mt-12">
 
           <div className="bg-white rounded-2xl overflow-hidden shadow-lg">
-            <Link to={`/produst`} onClick={scrollToTop}>            
-            <img
-              src="https://images.unsplash.com/photo-1521572267360-ee0c2909d518?w=700"
-              className="h-72 w-full object-cover"
-            />
+            <Link to={`/produst`} onClick={scrollToTop}>
+              <img
+                src="https://images.unsplash.com/photo-1521572267360-ee0c2909d518?w=700"
+                className="h-72 w-full object-cover"
+              />
             </Link>
 
             <div className="p-6">
@@ -87,12 +90,14 @@ function scrollToTop() {
             </div>
           </div>
 
-          <div className="bg-white rounded-2xl overflow-hidden shadow-lg">
-            
-            <img
-              src="https://images.unsplash.com/photo-1483985988355-763728e1935b?w=700"
-              className="h-72 w-full object-cover"
-            />
+          <div className=" bg-white rounded-2xl overflow-hidden shadow-lg">
+            <Link to={`/produst`} onClick={scrollToTop}>
+
+              <img
+                src="https://images.unsplash.com/photo-1483985988355-763728e1935b?w=700"
+                className="h-72 w-full object-cover"
+              />
+            </Link>
 
             <div className="p-6">
               <h3 className="font-bold text-2xl">
@@ -106,10 +111,13 @@ function scrollToTop() {
           </div>
 
           <div className="bg-white rounded-2xl overflow-hidden shadow-lg">
-            <img
-              src="https://images.unsplash.com/photo-1517841905240-472988babdf9?w=700"
-              className="h-72 w-full object-cover"
-            />
+            <Link to={`/produst`} onClick={scrollToTop}>
+
+              <img
+                src="https://images.unsplash.com/photo-1517841905240-472988babdf9?w=700"
+                className="h-72 w-full object-cover"
+              />
+            </Link>
 
             <div className="p-6">
               <h3 className="font-bold text-2xl">
