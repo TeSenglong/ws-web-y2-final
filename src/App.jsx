ca72144ff4967cb92ca921914ff5de0a455465f3
kansokhavathana-prog <kansokhavathana@gmail.com>
2026-07-13 13:23:39 +0700

    login Dashboard Register Orders Product and add importsomething in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Homepage from './page/home'
-import { Outlet, Route, Router, Routes } from 'react-router-dom'
-import About from './page/about'
-import Navbar from './components/navbar'
-import Footer from './components/footer'
-import ContactPage from './page/contact'
-import LoginPage from './page/login'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Homepage from "./page/home";
+import { Outlet, Route, Router, Routes } from "react-router-dom";
+import About from "./page/about";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import ContactPage from "./page/contact";
+import LoginPage from "./page/login";
+import Registerpage from "./page/Register";
+import Dashboard from "./page/Dashboard";
+import Product from "./page/Product";
+import Orders from "./page/Orders";
 
-import ProductBody from './page/products'
+import ProductBody from "./page/products";
 
 function App() {
   return (
-
     <Routes>
       {/* //mainlayout */}
-    <Route path='/' element ={<MainLayout/>} >
-      <Route path='/' element={<Homepage/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/produst' element={<ProductBody/>}/>
-      <Route path='/contact' element={<ContactPage/>}/>
-    </Route>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/produst" element={<ProductBody />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Route>
 
-    {/* login */}
-    <Route path='/login' element={<LoginPage/>} />
-
+      {/* login */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<Registerpage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/products" element={<Product />} />
+      <Route path="/orders" element={<Orders />} />
     </Routes>
-
+    
   );
 }
 
 export default App;
 
-function MainLayout(){
-  return(
+function MainLayout() {
+  return (
     <>
-    <Navbar/>
-    <Outlet/>
-    <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
 }
