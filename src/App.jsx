823431fdd8496ff73d720f22c304a428ad0643ce
kansokhavathana-prog <kansokhavathana@gmail.com>
2026-07-13 14:01:02 +0700

    resolve conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Homepage from './page/home'
-import { Outlet, Route, Router, Routes } from 'react-router-dom'
-import About from './page/about'
-import Navbar from './components/navbar'
-import Footer from './components/footer'
-import ContactPage from './page/contact'
-import LoginPage from './page/login'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Homepage from "./page/home";
+import { Outlet, Route, Router, Routes } from "react-router-dom";
+import About from "./page/about";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import ContactPage from "./page/contact";
+import LoginPage from "./page/login";
+import Registerpage from "./page/Register";
+import Dashboard from "./page/Dashboard";
+import Product from "./page/Product";
+import Orders from "./page/Orders";
 
-import ProductBody from './page/products'
+import ProductBody from "./page/products";
 
 function App() {
   return (
@@ -31,7 +35,6 @@ function App() {
       <Route path="/products" element={<Product />} />
       <Route path="/orders" element={<Orders />} />
     </Routes>
-    
   );
 }
 
